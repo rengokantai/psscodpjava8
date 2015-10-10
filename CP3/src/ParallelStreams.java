c2e7392be33c07db2d4c9adfbc5a28a8154e6e5f
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-10 11:42:58 -0400

    "cp3 ArrayList vs CopyOnWriteArrayList"

--- CP3/src/ParallelStreams.java
+++ CP3/src/ParallelStreams.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Stream;
 
 /**
@@ -7,6 +10,11 @@ public class ParallelStreams {
     public static void main(String[] args){
 
         System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism","2");
+
+        List<String> strings = new ArrayList<>();//may cause race condition
+        List<String> stringsafe = new CopyOnWriteArrayList<>(); //Thread safe but terrible performance (concurrent)
         Stream.iterate("+",s->s+"+").parallel().limit(6).peek(s->System.out.print(s +" work at "+ Thread.currentThread().getName())).forEach(System.out::println);
+        Stream.iterate("+",s->s+"+").parallel().limit(1000).forEach(s -> strings.add(s));
+        System.out.println(strings.size());
     }
 }
