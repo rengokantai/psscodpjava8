be77b7acdd9636897567bfade2c4b430c58ab88c
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-09 22:34:54 -0400

    "cp2 stream.flatMap(splitter)"

--- CP2/src/FlatMapExamples.java
+++ CP2/src/FlatMapExamples.java
@@ -3,6 +3,8 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 /**
@@ -17,10 +19,20 @@ public class FlatMapExamples {
         Stream<String> stream3 = Files.lines(Paths.get("CP2/3.txt"));
 
         Stream<Stream<String>> streamOfStreams = Stream.of(stream1,stream2,stream3);
-        System.out.println(streamOfStreams.count());
+        //Stream<String> streamOfLines = streamOfStreams.flatMap(stream -> stream);
+        Stream<String> streamOfLines = streamOfStreams.flatMap(Function.identity());
 
-        System.out.println(stream1.count());
-        System.out.println(stream2.count());
-        System.out.println(stream3.count());
+
+        Function<String, Stream<String>> lineSplitter = line -> Pattern.compile(" ").splitAsStream(line);
+
+        Stream<String> streamOfWords = streamOfLines.flatMap(lineSplitter).map(word->word.toLowerCase()).filter(word->word.length()==4).distinct();
+       // System.out.println(streamOfLines.count());
+        System.out.println(streamOfWords.count());
+
+
+//        System.out.println(streamOfStreams.count());
+//        System.out.println(stream1.count());
+//        System.out.println(stream2.count());
+//        System.out.println(stream3.count());
     }
 }
