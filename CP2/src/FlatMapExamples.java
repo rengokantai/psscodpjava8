4d96656f265351d2f5cd14d36eab200a7f9ea498
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-09 22:17:33 -0400

    "cp2 stream.of()"

--- CP2/src/FlatMapExamples.java
+++ CP2/src/FlatMapExamples.java
@@ -16,6 +16,9 @@ public class FlatMapExamples {
         Stream<String> stream2 = Files.lines(Paths.get("CP2/2.txt"));
         Stream<String> stream3 = Files.lines(Paths.get("CP2/3.txt"));
 
+        Stream<Stream<String>> streamOfStreams = Stream.of(stream1,stream2,stream3);
+        System.out.println(streamOfStreams.count());
+
         System.out.println(stream1.count());
         System.out.println(stream2.count());
         System.out.println(stream3.count());
