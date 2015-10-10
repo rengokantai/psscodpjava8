f108aa864a5388e4e45def17649f14ebc765975d
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-10 11:49:51 -0400

    "cp3 collect(Collectors.toList())"

--- CP3/src/ParallelStreams.java
+++ CP3/src/ParallelStreams.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -15,6 +17,8 @@ public class ParallelStreams {
         List<String> stringsafe = new CopyOnWriteArrayList<>(); //Thread safe but terrible performance (concurrent)
         Stream.iterate("+",s->s+"+").parallel().limit(6).peek(s->System.out.print(s +" work at "+ Thread.currentThread().getName())).forEach(System.out::println);
         Stream.iterate("+",s->s+"+").parallel().limit(1000).forEach(s -> strings.add(s));
+        List<String> collect = Stream.iterate("+", s -> s + "+").parallel().limit(1000).collect(Collectors.toList());
         System.out.println(strings.size());
+        System.out.println(collect.size());
     }
 }
